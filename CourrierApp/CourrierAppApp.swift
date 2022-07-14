d465dff84b0a573f3ad7345d0a305ae549fea471
EZENGOLD <noudekiki@gmail.com>
2022-07-14 10:05:08 +0100

    Initial Commit

---
+++ CourrierApp/CourrierAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourrierAppApp.swift
+//  CourrierApp
+//
+//  Created by ezen on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CourrierAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
